ca0e36eea8500ece56cfa48524d0151230d1c48d
Daniel Pericich <dpericich@gmail.com>
2023-11-10 14:58:06 -0600

    Scaffolded category element component

---
+++ src/app/results/categoryElement.tsx
@@ -0,0 +1,16 @@
+const CategoryElement = () => {
+    return(
+        <div className="">
+            <div className="">Category Title</div>
+            <div className="">
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                </ul>
+            </div>
+        </div>
+    )
+};
+
+export default CategoryElement;
