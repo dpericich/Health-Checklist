c94941c1aa08dc62716b364e9d85f4d78d205485
Taylor <mccoyt57@gmail.com>
2023-12-20 18:46:42 -0600

    Adjusted some styling in summary section

--- src/components/summary/summarySection.tsx
+++ src/components/summary/summarySection.tsx
@@ -5,22 +5,25 @@ type Props = {
     dataKey: string;
 }
 const MainSummarySection = ({ dataKey } : Props) => {
-const summaryData = healthData[dataKey]?.summary;
-
-    return (
-        <div className="grid card p-6 border-2 border-primary-font-color border-solid rounded-box shadow-lg">
+    const summaryData = healthData[dataKey]?.summary;
+return(
+   <div className="grid card p-6 border-2 border-primary-font-color border-solid rounded-box shadow-lg">
             {summaryData && (
                 <>
-                    <p>{summaryData.description}</p>
-                    <ul>
-                        {summaryData.keyPoints.map((point, index) => (
-                            <li key={index}>{point}</li>
-                        ))}
-                    </ul>
+                    <p style={{fontSize: 25}}>{summaryData.description}</p>
+                    <br />
+                    <p style={{ textAlign: 'center', fontSize: 25 }}>Points to remember:</p>
+                    {summaryData.keyPoints.length > 0 && (
+                        <ul style={{ textAlign: 'center', fontSize: 25 }}>
+                            {summaryData.keyPoints.map((point, index) => (
+                                <li key={index}>{point}</li>
+                            ))}
+                        </ul>
+                    )}
                 </>
             )}
         </div>
-    );
+)
 }
 
 export default MainSummarySection;
