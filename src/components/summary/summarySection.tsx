973621625dab49f8e4b5c92e0d01f29179694cee
Taylor <mccoyt57@gmail.com>
2023-12-23 10:17:41 -0600

    Updated styling on main summary

--- src/components/summary/summarySection.tsx
+++ src/components/summary/summarySection.tsx
@@ -10,13 +10,13 @@ return(
    <div className="grid card p-6 border-2 border-primary-font-color border-solid rounded-box shadow-lg">
             {summaryData && (
                 <>
-                    <p style={{fontSize: 25}}>{summaryData.description}</p>
+                    <p className='text-xl'>{summaryData.description}</p>
                     <br />
-                    <p style={{ textAlign: 'center', fontSize: 25 }}>Points to remember:</p>
+                    <p className='text-center text-3xl mb-6'>Points to remember:</p>
                     {summaryData.keyPoints.length > 0 && (
-                        <ul style={{ textAlign: 'center', fontSize: 25 }}>
+                        <ul className='text-xl m-auto'>
                             {summaryData.keyPoints.map((point, index) => (
-                                <li key={index}>{point}</li>
+                                <li key={index}>- {point}</li>
                             ))}
                         </ul>
                     )}
