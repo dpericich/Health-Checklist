a0d39eb7aa5ec89476e564c2838f6f40867ddc05
Scott L <ehee27@gmail.com>
2024-02-15 22:17:43 -0600

    modal margins and scroll implemented

--- src/components/modal/MarkerModal.tsx
+++ src/components/modal/MarkerModal.tsx
@@ -46,64 +46,58 @@ const MarkerModal = ({ openMarker, onClose, item }: any) => {
   })
 
   return (
-    <div className={styles.modalContainer}>
-      <div className="">
-        <button
-          className="absolute top-5 right-5 btn btn-sm btn-neutral w-[40px] text-white transition ease-in-out delay-25 hover:scale-105 font-sans"
-          onClick={onClose}
-        >
-          X
-        </button>
+    // <div className={styles.modalContainer}>
+    <div
+      className={`fixed inset-0 m-10 mt-20 max-h-[70%] flex flex-col justify-center items-center rounded-md shadow-md p-2 transition-colors ${
+        openMarker ? 'visible bg-gray-200 z-50' : 'invisible'
+      }`}
+    >
+      <div className="flex gap-2 items-center text-3xl md:text-7xl my-10">
+        {iconMappings[`${item.type}`]}
+
+        <span className="text-2xl md:text-5xl">{item.name}</span>
       </div>
-      <div className="flex gap-2 justify-center items-center text-7xl w-[100%]">
-        {/* ------- HEADER --------------------------- */}
-
-        <div className="border-2 flex gap-2 items-center">
-          <div className="text-3xl md:text-7xl">
-            {iconMappings[`${item.type}`]}
+      <div className="flex flex-col items-center h-10/12 overflow-y-scroll bg-gray-100 shadow-inner shadow-gray-500 rounded-md py-10">
+        {item.aboveAverage ? (
+          <div className="border-4 w-3/4">
+            <BarChart chartData={data} />
           </div>
-          <span className="text-2xl md:text-5xl">{item.name}</span>
-        </div>
+        ) : (
+          <div className="flex flex-col justify-center items-center my-4 bg-white shadow-inner shadow-gray-300 rounded-md p-2 w-[80%] md:w-[50%]">
+            <h1 className="text-xl font-sans">Average</h1>
+
+            {/* ------- CHART / AVERAGE ---------------------- */}
+            <div className="font-sans text-lg md:text-2xl">
+              {item.average} {item.units}
+            </div>
+          </div>
+        )}
+
+        {/* ------- ACCORDIAN ---------------------- */}
+        <Accordion item={item} />
+        {item.videoThumb && (
+          <div className="flex flex-col gap-4 justify-center items-center p-3 my-2 w-full">
+            <p className="text-lg font-sans">Click Image for Assessment</p>
+            <a href={item.videoURL} target="_blank">
+              <Image
+                src={`/${item.videoThumb}`}
+                alt="video-thumb"
+                width={200}
+                height={200}
+                className="rounded shadow-md shadow-gray-400 border-2 border-transparent hover:border-green-500 hover:scale-105 transition-all"
+              />
+            </a>
+          </div>
+        )}
       </div>
-      {/* </div> */}
-      {/* ------- CHART / AVERAGE ---------------------- */}
-      {item.aboveAverage ? (
-        <div className="w-[85%] md:w-[70%] my-4 bg-white shadow-inner shadow-gray-300 rounded-md p-2">
-          <BarChart chartData={data} />
-        </div>
-      ) : (
-        <div className="flex flex-col justify-center items-center my-4 bg-white shadow-inner shadow-gray-300 rounded-md p-2 w-[80%] md:w-[50%]">
-          <h1 className="text-xl font-sans">Average</h1>
-
-          <div className="font-sans text-lg md:text-2xl">
-            {item.average} {item.units}
-          </div>
-        </div>
-      )}
-      {/* ------- ACCORDIAN ---------------------- */}
-      <Accordion item={item} />
-      {item.videoThumb && (
-        <div className="flex flex-col gap-4 justify-center items-center p-3 my-2 w-full">
-          <p className="text-lg font-sans">Click Image for Assessment</p>
-          <a href={item.videoURL} target="_blank">
-            <Image
-              src={`/${item.videoThumb}`}
-              alt="video-thumb"
-              width={200}
-              height={200}
-              className="rounded shadow-md shadow-gray-400 border-2 border-transparent hover:border-green-500 hover:scale-105 transition-all"
-            />
-          </a>
-        </div>
-      )}
-
-      {/* <button
-        className="btn btn-sm btn-neutral absolute top-6 right-2 md:top-5 md:right-5 w-[40px] text-white  transition ease-in-out delay-25 hover:scale-105 shadow-md shadow-gray-400 font-sans"
+      <button
+        className="absolute top-5 right-5 btn btn-sm btn-neutral w-[40px] text-white transition ease-in-out delay-25 hover:scale-105 font-sans"
         onClick={onClose}
       >
         X
-      </button> */}
+      </button>
     </div>
+    // </div>
   )
 }
 
